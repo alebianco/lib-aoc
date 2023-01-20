8898675cc4c3af044053458d8b1d41d5151569f0
Alessandro Bianco <code@alebianco.dev>
2023-01-20 21:25:26 +0100

    refactor(itertools): extend slice functionalities
    slice now supports negative values

--- src/itertools/slice.js
+++ src/itertools/slice.js
@@ -1,4 +1,5 @@
 import { isIterable } from './isIterable.js'
+import { from } from './from.js'
 
 /**
  * Returns a sequence of selected elements from the {@link source}.
@@ -13,41 +14,187 @@ import { isIterable } from './isIterable.js'
  * slice([1, 2, 3, 4, 5], 1, 4); // returns [2, 3, 4]
  * @example
  * slice([1, 2, 3, 4, 5], 1, 4, 2); // returns [2, 4]
+ * @example
+ * slice([1, 2, 3, 4, 5], 1, -2); // returns [2, 3]
+ * @example
+ * slice([1, 2, 3, 4, 5], 4, 1, -2); // returns [5, 3]
  *
+ * @requires module:itertools.from
  * @requires module:itertools.isIterable
  *
  * @template T
  * @param {Iterable<T>} source where to take the elements from
- * @param {number} [start=0] number of elements to take before terminating the sequence
- * @param {number} [stop=Number.POSITIVE_INFINITY] number of elements to take before terminating the sequence
+ * @param {number} [start] number of elements to take before terminating the sequence, default to 0 when going forward, -1 when going backward
+ * @param {number} [stop] number of elements to take before terminating the sequence
  * @param {number} [step=1] number of elements to take before terminating the sequence
  * @yields {T} the next value of the source
  * @returns {Generator<T, void, void>} the sequence generator
  *
  * @throws {TypeError} when {@link source} is not an {@link Iterable}
- * @throws {TypeError} when {@link start} is not a whole number
- * @throws {TypeError} when {@link stop} is not a whole number
- * @throws {TypeError} when {@link step} is not a whole number or is equal to 0
+ * @throws {TypeError} when {@link start} is not an integer number or undefined
+ * @throws {TypeError} when {@link stop} is not an integer number or undefined
+ * @throws {TypeError} when {@link step} is not an integer number or is equal to 0
  */
-export function * slice (source, start = 0, stop = Number.POSITIVE_INFINITY, step = 1) {
+export function * slice (source, start = undefined, stop = undefined, step = 1) {
   if (!isIterable(source)) {
     throw new TypeError(`Argument 'source' should be iterable, received ${source} (${typeof source}).`)
   }
-  if (!Number.isInteger(start) || start < 0) {
-    throw new TypeError(`Argument 'limit' should be a whole number, received ${start} (${typeof start}).`)
+  if (!Number.isInteger(start) && start !== undefined) {
+    throw new TypeError(`Argument 'start' should be an integer number, received ${start} (${typeof start}).`)
   }
-  if (stop !== Number.POSITIVE_INFINITY && (!Number.isInteger(stop) || stop < 0)) {
-    throw new TypeError(`Argument 'stop' should be a whole number, received ${stop} (${typeof stop}).`)
+  if (!Number.isInteger(stop) && stop !== undefined) {
+    throw new TypeError(`Argument 'stop' should be an integer number, received ${stop} (${typeof stop}).`)
   }
-  if (!Number.isInteger(step) || step <= 0) {
-    throw new TypeError(`Argument 'step' should be a whole number different from 0, received ${step} (${typeof step}).`)
+  if (!Number.isInteger(step) || step === 0) {
+    throw new TypeError(`Argument 'step' should be a positive or negative integer, different from 0, received ${step} (${typeof step}).`)
   }
 
-  let index = 0
-  for (const value of source) {
-    if (index >= start && index < stop && (index - start) % step === 0) {
-      yield value
+  if (step > 0) yield * forward(from(source), start, stop, step)
+  else yield * backward(from(source), start, stop, step)
+}
+
+function * forward (source, start, stop, step) {
+  start = start ?? 0
+
+  if (start < 0) {
+    // track total length of the original source
+    let length = 0
+
+    // buffer all values except the last `-start` values
+    const buffer = []
+    for (let element = source.next(); element.done === false; element = source.next()) {
+      length++
+      buffer.push(element.value)
+      if (buffer.length > -start) {
+        buffer.shift()
+      }
     }
-    index++
+
+    // adjust `start` and `stop` to be positive
+    start = Math.max(length + start, 0)
+    if (stop === undefined) {
+      stop = length
+    } else if (stop >= 0) {
+      stop = Math.min(stop, length)
+    } else {
+      stop = Math.max(length + stop, 0)
+    }
+
+    // regular slice of the buffer values
+    const count = stop - start
+    if (count > 0) {
+      const it = from(buffer)
+      for (let index = 0, element = it.next(); index < count && element.done === false; index++, element = it.next()) {
+        // yield values at the right `step` interval
+        if (index % step === 0) {
+          yield element.value
+        }
+      }
+    }
+    // clear buffer
+    buffer.length = 0
+  } else if (stop !== undefined && stop < 0) {
+    // we won't need values up to `start`
+    for (let index = 0; index < start; index++) {
+      const { done } = source.next()
+      if (done) break
+    }
+
+    // slice while buffering and delay `-stop` values
+    const buffer = []
+    for (let index = 0, element = source.next(); element.done === false; index++, element = source.next()) {
+      buffer.push(element.value)
+      if (buffer.length > -stop) {
+        const element = buffer.shift()
+        // yield values at the right `step` interval
+        if ((index + stop) % step === 0) {
+          yield element
+        }
+      }
+    }
+    // clear buffer
+    buffer.length = 0
+  } else {
+    stop = stop ?? Number.POSITIVE_INFINITY
+
+    // regular slicing
+    for (let index = 0, element = source.next(); element.done === false; index++, element = source.next()) {
+      // yield values in the requested range at the right `step` interval
+      if (index >= start && index < stop && (index - start) % step === 0) {
+        yield element.value
+      }
+    }
+  }
+}
+
+function * backward (source, start, stop, step) {
+  start = start ?? -1
+
+  if (stop !== undefined && stop < 0) {
+    // track total length of the original source
+    let length = 0
+
+    // buffer (in reverse order) all values except the last `-stop-1`
+    const buffer = []
+    for (let index = 1, element = source.next(); element.done === false; index++, element = source.next()) {
+      length = index
+      buffer.unshift(element.value)
+      if (buffer.length > (-stop - 1)) {
+        buffer.pop()
+      }
+    }
+
+    if (start >= 0) {
+      // adjust `start` to be negative
+      start = Math.min(start - length, -1)
+    }
+
+    // yield items from buffer up to `start` or end
+    const it = from(buffer)
+    for (let index = 0, element = it.next(); element.done === false; index--, element = it.next()) {
+      if (index <= (start + 1) && (index + start) % step === 0) {
+        // yield values in the requested range at the right `step` interval
+        yield element.value
+      }
+    }
+    // clear buffer
+    buffer.length = 0
+  } else {
+    let it = source
+
+    if (stop !== undefined) {
+      // we won't need values up to `stop`
+      for (let index = 0; index <= stop; index++) {
+        const { done } = it.next()
+        if (done) break
+      }
+    }
+
+    if (start < 0) {
+      // cache all the remaining values until the end, then adjust `start` to be positive
+      const cache = Array.from(it)
+      stop = 0
+      start = start + cache.length + 1
+      // now operate over the cached values
+      it = from(cache)
+    } else if (stop === undefined) {
+      // we want values up to `start`
+      stop = 0
+      start = start + 1
+    }
+
+    if (start > stop) {
+      const buffer = []
+      for (let index = stop, element = it.next(); index < start && element.done === false; index++, element = it.next()) {
+        if (index >= stop && (index - start + stop) % step === 0) {
+          // buffer values (in reversed order) in the requested range at the right `step` interva
+          buffer.unshift(element.value)
+        }
+      }
+      // yield all values
+      yield * buffer
+      // clear buffer
+      buffer.length = 0
+    }
   }
 }
--- src/itertools/slice.test.js
+++ src/itertools/slice.test.js
@@ -1,8 +1,15 @@
 import assert from 'node:assert'
 import { describe, it } from 'node:test'
 import { slice } from './slice.js'
-import { naturals } from './numbers.js'
-import { take } from './take.js'
+import { count } from './count.js'
+
+function executeCases (cases) {
+  cases.forEach(([args, expected, options = {}]) => {
+    it(`slice(${args.map(x => JSON.stringify(x)).join(', ')}) -> "${expected}"`, options, () => {
+      assert.deepStrictEqual([...slice.apply(null, args)], expected.split(''))
+    })
+  })
+}
 
 describe('slice', () => {
   it('accepts only iterables', () => {
@@ -12,40 +19,136 @@ describe('slice', () => {
     assert.doesNotThrow(() => slice([1, 2, 3]).next())
   })
 
-  it('accepts a whole number as start position', () => {
-    assert.throws(() => slice('abc', -1).next(), TypeError)
+  it('accepts a integer number or undefined as start position', () => {
     assert.throws(() => slice('abc', 1.5).next(), TypeError)
     assert.throws(() => slice('abc', Number.NaN).next(), TypeError)
+    assert.doesNotThrow(() => slice('abc', undefined).next())
     assert.doesNotThrow(() => slice('abc', 1).next())
+    assert.doesNotThrow(() => slice('abc', 0).next())
+    assert.doesNotThrow(() => slice('abc', -1).next())
   })
 
-  it('accepts a whole number as stop position', () => {
-    assert.throws(() => slice('abc', 0, -1).next(), TypeError)
+  it('accepts a integer number or undefined as stop position', () => {
     assert.throws(() => slice('abc', 0, 1.5).next(), TypeError)
     assert.throws(() => slice('abc', 0, Number.NaN).next(), TypeError)
+    assert.doesNotThrow(() => slice('abc', 0, undefined).next())
     assert.doesNotThrow(() => slice('abc', 0, 1).next())
+    assert.doesNotThrow(() => slice('abc', 0, 0).next())
+    assert.doesNotThrow(() => slice('abc', 0, -1).next())
   })
 
-  it('accepts a natural number as step', () => {
-    assert.throws(() => slice('abc', 0, 1, -1).next(), TypeError)
+  it('accepts a integer number except 0 as step', () => {
+    assert.throws(() => slice('abc', 0, 1, 0).next(), TypeError)
     assert.throws(() => slice('abc', 0, 1, 1.5).next(), TypeError)
     assert.throws(() => slice('abc', 0, 1, Number.NaN).next(), TypeError)
     assert.doesNotThrow(() => slice('abc', 0, 1, 1).next())
+    assert.doesNotThrow(() => slice('abc', 0, 1, -1).next())
   })
 
-  it('returns an empty sequence if stop is lower than start', () => {
-    assert.deepStrictEqual([...slice('abc', 2, 1)], [])
+  describe('going forward', function () {
+    describe('should remove the requested amount of values', () => {
+      executeCases([
+        [['abcdefgh', 0], 'abcdefgh'],
+        [['abcdefgh', 2], 'cdefgh'],
+        [['abcdefgh', 10], ''],
+        [['abcdefgh', -2], 'gh'],
+        [['abcdefgh', -10], 'abcdefgh']
+      ])
+    })
+
+    describe('should keep values up to the requested amount', () => {
+      executeCases([
+        [['abcdefgh', undefined, 0], ''],
+        [['abcdefgh', undefined, 2], 'ab'],
+        [['abcdefgh', undefined, 10], 'abcdefgh'],
+        [['abcdefgh', undefined, -2], 'abcdef'],
+        [['abcdefgh', undefined, -10], ''],
+        [['abcdefgh', 0, 0], ''],
+        [['abcdefgh', 0, 2], 'ab'],
+        [['abcdefgh', 0, 10], 'abcdefgh'],
+        [['abcdefgh', 0, -2], 'abcdef'],
+        [['abcdefgh', 0, -10], ''],
+        [['abcdefgh', 2, 0], ''],
+        [['abcdefgh', 2, 2], ''],
+        [['abcdefgh', 2, 10], 'cdefgh'],
+        [['abcdefgh', 2, -2], 'cdef'],
+        [['abcdefgh', 2, -10], ''],
+        [['abcdefgh', 10, 0], ''],
+        [['abcdefgh', 10, 2], ''],
+        [['abcdefgh', 10, 10], ''],
+        [['abcdefgh', 10, -2], ''],
+        [['abcdefgh', 10, -10], ''],
+        [['abcdefgh', -2, 0], ''],
+        [['abcdefgh', -2, 2], ''],
+        [['abcdefgh', -2, 10], 'gh'],
+        [['abcdefgh', -2, -2], ''],
+        [['abcdefgh', -2, -10], ''],
+        [['abcdefgh', -10, 0], ''],
+        [['abcdefgh', -10, 2], 'ab'],
+        [['abcdefgh', -10, 10], 'abcdefgh'],
+        [['abcdefgh', -10, -2], 'abcdef'],
+        [['abcdefgh', -10, -10], '']
+      ])
+    })
   })
 
-  it('can skip the initial values', () => {
-    assert.deepStrictEqual([...slice('abcdef', 3)], ['d', 'e', 'f'])
+  describe('going backward', function () {
+    describe('should remove the requested amount of values', () => {
+      executeCases([
+        [['abcdefgh', 0, undefined, -1], 'a'],
+        [['abcdefgh', 2, undefined, -1], 'cba'],
+        [['abcdefgh', 10, undefined, -1], 'hgfedcba'],
+        [['abcdefgh', -2, undefined, -1], 'gfedcba'],
+        [['abcdefgh', -10, undefined, -1], '']
+      ])
+    })
+
+    describe('should keep values up to the requested amount', () => {
+      executeCases([
+        [['abcdefgh', undefined, 0, -1], 'hgfedcb'],
+        [['abcdefgh', undefined, 2, -1], 'hgfed'],
+        [['abcdefgh', undefined, 10, -1], ''],
+        [['abcdefgh', undefined, -2, -1], 'h'],
+        [['abcdefgh', undefined, -10, -1], 'hgfedcba'],
+        [['abcdefgh', 0, 0, -1], ''],
+        [['abcdefgh', 0, 2, -1], ''],
+        [['abcdefgh', 0, 10, -1], ''],
+        [['abcdefgh', 0, -2, -1], ''],
+        [['abcdefgh', 0, -10, -1], 'a'],
+        [['abcdefgh', 2, 0, -1], 'cb'],
+        [['abcdefgh', 2, 2, -1], ''],
+        [['abcdefgh', 2, 10, -1], ''],
+        [['abcdefgh', 2, -2, -1], ''],
+        [['abcdefgh', 2, -10, -1], 'cba'],
+        [['abcdefgh', 10, 0, -1], 'hgfedcb'],
+        [['abcdefgh', 10, 2, -1], 'hgfed'],
+        [['abcdefgh', 10, 10, -1], ''],
+        [['abcdefgh', 10, -2, -1], 'h'],
+        [['abcdefgh', 10, -10, -1], 'hgfedcba'],
+        [['abcdefgh', -2, 0, -1], 'gfedcb'],
+        [['abcdefgh', -2, 2, -1], 'gfed'],
+        [['abcdefgh', -2, 10, -1], ''],
+        [['abcdefgh', -2, -2, -1], ''],
+        [['abcdefgh', -2, -10, -1], 'gfedcba'],
+        [['abcdefgh', -10, 0, -1], ''],
+        [['abcdefgh', -10, 2, -1], ''],
+        [['abcdefgh', -10, 10, -1], ''],
+        [['abcdefgh', -10, -2, -1], ''],
+        [['abcdefgh', -10, -10, -1], '']
+      ])
+    })
   })
 
-  it('can stop before the source end', () => {
-    assert.deepStrictEqual([...slice('abcdef', 0, 3)], ['a', 'b', 'c'])
+  describe('can skip values', function () {
+    executeCases([
+      [['abcdefgh', 1, 6, 3], 'be'],
+      [['abcdefgh', 6, 1, -3], 'gd'],
+      [['abcdefgh', 1, -2, 3], 'be'],
+      [['abcdefgh', 6, -6, -3], 'gd']
+    ])
   })
 
-  it('can skip some values', () => {
-    assert.deepStrictEqual([...slice(take(naturals(), 100), 10, 50, 7)], [10, 17, 24, 31, 38, 45])
+  it('can (carefully) slice backward an infinite sequence', function () {
+    assert.deepStrictEqual([...slice(count(), 110, 99, -2)], [110, 108, 106, 104, 102, 100])
   })
 })
